270819ebcbb89468305e8ab47bc070239d702037
Noa Le Roux Mbaye <noambaye@gmail.com>
2025-04-01 14:19:08 +0200

    Update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,31 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+--[[ return {
+  {
+    {
+      '3rd/image.nvim',
+      opts = {},
+    },
+  },
+  {
+    rocks = {
+      hererocks = false, -- recommended if you do not have global installation of Lua 5.1.
+    },
+  },
+}
+--]]
+
+return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        ruby_lsp = {
+          mason = false,
+          cmd = { vim.fn.expand '~/.rbenv/shims/ruby-lsp' },
+        },
+      },
+    },
+  },
+}
